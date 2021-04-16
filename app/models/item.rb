fce002c8f5caa28ab8b50ba5db889bae5ca87c34
ks251111 <k.11t596@gmail.com>
2021-04-16 22:54:00 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :title
+    validates :text
+    validates :category_id
+    validates :status_id
+    validates :burden_id
+    validates :area_id
+    validates :day_id
+    validates :price
+  end
 end
