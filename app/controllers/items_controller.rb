114f9e0deaea5c639d81fd9b50115dea1f77931c
ks251111 <k.11t596@gmail.com>
2021-04-17 12:02:09 +0900

    newアクションとcreateアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,9 +3,16 @@ class ItemsController < ApplicationController
   end
   
   def new
+    @item = Item.new
   end
 
   def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+  params.require(:item).permit(:image, :title, :text, :category_id, :status_id, :burden_id, :area_id, :day_id, :price)
   end
   
 end
