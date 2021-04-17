1262f890f38e1bb1902582dbb9fd65e2d6e84499
ks251111 <k.11t596@gmail.com>
2021-04-17 20:55:55 +0900

    ActiveHashの導入２

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,13 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :burden
+  belongs_to :area
+  belongs_to :day
+
   with_options presence: true do
     validates :image
     validates :title
@@ -12,5 +19,13 @@ class Item < ApplicationRecord
     validates :area_id
     validates :day_id
     validates :price
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :burden_id
+    validates :area_id
+    validates :day_id
   end
 end
