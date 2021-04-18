54def40dab5f3dbeb6fb8809ab89d0069cf7c0e1
ks251111 <k.11t596@gmail.com>
2021-04-18 12:06:07 +0900

    コントローラーとビューファイルの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,12 +8,17 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
   def item_params
-  params.require(:item).permit(:image, :title, :text, :category_id, :status_id, :burden_id, :area_id, :day_id, :price)
+    params.require(:item).permit(:image, :title, :text, :category_id, :status_id, :burden_id, :area_id, :day_id, :price).merge(user_id: current_user.id)
   end
   
 end
