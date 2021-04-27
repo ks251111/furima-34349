d0de2d7142f246993908391734e06affae758e33
ks251111 <k.11t596@gmail.com>
2021-04-27 20:38:58 +0900

    クレジット決済機能の実装

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -9,6 +9,7 @@ class BuysController < ApplicationController
     @item = Item.find(params[:item_id])
     @buy_shipping = BuyShipping.new(buy_params)
     if @buy_shipping.valid?
+      pay_item
       @buy_shipping.save
       redirect_to root_path
     else
@@ -19,6 +20,15 @@ class BuysController < ApplicationController
   private
 
   def buy_params
-    params.require(:buy_shipping).permit(:postal, :area, :city, :address, :build, :tel).merge(user_id: current_user.id)
+    params.require(:buy_shipping).permit(:postal, :area_id, :city, :address, :build, :tel).merge(user_id: current_user.id, token: params[:token], item_id: params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item[:price],
+        card: buy_params[:token],
+        currency: 'jpy'
+      )
   end
 end
--- app/models/buy_shipping.rb
+++ app/models/buy_shipping.rb
@@ -1,20 +1,20 @@
 class BuyShipping
   include ActiveModel::Model
-  attr_accessor :postal, :area, :city, :address, :build, :tel, :user_id, :item_id
+  attr_accessor :postal, :area_id, :city, :address, :build, :tel, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal
     validates :city
     validates :address
-    validates :build
     validates :tel
     validates :user_id
     validates :item_id
+    validates :token
   end
-  validates :area, numericality: {other_than: 1}
+  validates :area_id, numericality: {other_than: 1}
 
   def save
     buy = Buy.create(user_id: user_id, item_id: item_id)
-    Shipping.create(postal: postal, area: area, city: city, address: address, build: build, tel: tel, buy_id: buy.id)
+    Shipping.create(postal: postal, area: area_id, city: city, address: address, build: build, tel: tel, buy_id: buy.id)
   end
 end
