01596153e0135d24d20d785fb881fc9591c91834
ks251111 <k.11t596@gmail.com>
2021-04-28 19:39:09 +0900

    buy_shippingモデルのテスト

---
+++ spec/factories/buy_shippings.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :buy_shipping do
+    postal  { '123-4567' }
+    area_id { 2 }
+    city    { '横浜市緑区' }
+    address { '青山1-1-1' }
+    build   { '柳ビル103' }
+    tel     { '09012345678' }
+    token   { 'tok_abcdefghijk00000000000000000' }
+    item_id { '10' }
+  end
+end
