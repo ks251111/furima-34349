d115dfe853af7b96137f39daccd46554a96a7861
ks251111 <k.11t596@gmail.com>
2021-05-02 18:21:02 +0900

    お気に入り機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :items do
     resources :buys, only: [:index, :create]
     resources :comments, only: :create
+    resources :likes, only: [:create, :destroy]
     collection do
       get 'search'
     end
