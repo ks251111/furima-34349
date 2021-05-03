e8e3c15a6309e1f3d97d8bd4e9425b120a0285db
ks251111 <k.11t596@gmail.com>
2021-05-03 17:28:31 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     resources :buys, only: [:index, :create]
     resources :comments, only: :create
     resources :likes, only: [:create, :destroy]
+    resources :reports, only: [:index, :create]
     collection do
       get 'search'
     end
