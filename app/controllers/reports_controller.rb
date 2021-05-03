bd8acddb0a234fd44a31cda9b1223f70c0e9ff74
ks251111 <k.11t596@gmail.com>
2021-05-03 21:26:21 +0900

    コントローラーの編集

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,14 +1,13 @@
 class ReportsController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
+  before_action :before_reports, only: [:index, :create]
   
   def index
-    @item = Item.find(params[:item_id])
     @report = Report.new
     @reports = @item.reports.includes(:user)
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @report = Report.create(report_params)
     if @report.save
       redirect_to item_path(@report.item)
@@ -23,5 +22,12 @@ class ReportsController < ApplicationController
 
   def report_params
     params.require(:report).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def before_reports
+    @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 end
