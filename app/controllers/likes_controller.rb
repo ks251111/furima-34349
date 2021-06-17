d83cfa5a519232b575cad134bdbb8aa819427cdc
ks251111 <k.11t596@gmail.com>
2021-06-17 19:44:41 +0900

    コントローラーの修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,26 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :like_params, only: [:create, :destroy]
 
   def create
-    @like = Like.create(user_id: current_user.id, item_id: params[:item_id])
-    redirect_to item_path(@like.item)
+    @like = current_user.likes.build(like_params)
+    @item = @like.item
+    if @like.save
+      respond_to :js
+    end
   end
 
   def destroy
-    @like = Like.find_by(user_id: current_user.id, item_id: params[:item_id]).destroy
-    redirect_to item_path(@like.item)
+    @like = Like.find_by(id: params[:id])
+    @item = @like.item
+    if @like.destroy
+      respond_to :js
+    end
   end
 
+  private
+
+  def like_params
+    params.permit(:item_id)
+  end
 end
